cf0c183cde17fc06436f6d08c38ec01f36544980
Andy Huynh <andy-huynh@hotmail.com>
2023-01-27 13:59:53 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [value, onChangeText] = React.useState('')
+  var hehehaha = 'haha'
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <Text>{hehehaha}</Text>
+      <TextInput
+        editable
+        placeholder='Input Text Here'
+        onChangeText={text => onChangeText(text)}
+        value={value}/>
+        <Button
+          title='Submit'
+          onPress={() => Alert.alert(value)}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +28,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 5 ,
   },
 });
